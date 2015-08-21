b772ebfe703967ab1165952da4264e3e5fa83e2e
bibendovsky <bibendovsky@hotmail.com>
2015-08-21 23:38:12 +0300

    Fix compiling errors in ltjs_lib_object

--- libs/lith/lithbaselist.h
+++ libs/lith/lithbaselist.h
@@ -40,7 +40,7 @@ class CLithBaseList
 {
 public:
     // Constructors and destructors
-    CLithBaseList() { m_pFirst = NULL; m_pLast = NULL; };
+    CLithBaseList() { m_pFirst = nullptr; m_pLast = nullptr; };
     ~CLithBaseList() { };
 
     // member insert and delete functions
@@ -63,29 +63,29 @@ private:
 
 template<class ItemType>
 void CLithBaseList<ItemType>::InsertFirst(ItemType* pItem) {
-  ASSERT(pItem != NULL);
+  ASSERT(pItem != nullptr);
   pItem->m_pNext = m_pFirst;
-  pItem->m_pPrev = NULL;
-  if (m_pFirst != NULL) m_pFirst->m_pPrev = pItem;
+  pItem->m_pPrev = nullptr;
+  if (m_pFirst) m_pFirst->m_pPrev = pItem;
   else m_pLast = pItem;
   m_pFirst = pItem;
 };
 
 template<class ItemType>
 void CLithBaseList<ItemType>::InsertLast(ItemType* pItem) {
-  ASSERT(pItem != NULL);
-  pItem->m_pNext = NULL;
+  ASSERT(pItem != nullptr);
+  pItem->m_pNext = nullptr;
   pItem->m_pPrev = m_pLast;
-  if (m_pLast != NULL) m_pLast->m_pNext = pItem;
+  if (m_pLast) m_pLast->m_pNext = pItem;
   else m_pFirst = pItem;
   m_pLast = pItem;
 };
 
 template<class ItemType>
 void CLithBaseList<ItemType>::InsertAfter(ItemType* pBeforeItem, ItemType* pNewItem) {
-  ASSERT(pNewItem != NULL);
-  if (pBeforeItem == NULL) InsertFirst(pNewItem); // Insert at start of list if pBeforeItem is NULL
-  if (pBeforeItem->m_pNext != NULL) pBeforeItem->m_pNext->m_pPrev = pNewItem;
+  ASSERT(pNewItem != nullptr);
+  if (!pBeforeItem) InsertFirst(pNewItem); // Insert at start of list if pBeforeItem is NULL
+  if (pBeforeItem->m_pNext) pBeforeItem->m_pNext->m_pPrev = pNewItem;
   else m_pLast = pNewItem;
   pNewItem->m_pPrev = pBeforeItem;
   pNewItem->m_pNext = pBeforeItem->m_pNext;
@@ -94,9 +94,9 @@ void CLithBaseList<ItemType>::InsertAfter(ItemType* pBeforeItem, ItemType* pNewI
 
 template<class ItemType>
 void CLithBaseList<ItemType>::InsertBefore(ItemType* pAfterItem, ItemType* pNewItem) {
-  ASSERT(pNewItem != NULL);
-  if (pAfterItem == NULL) InsertLast(pNewItem); // Insert at end of list if pAfterItem is NULL
-  if (pAfterItem->m_pPrev != NULL) pAfterItem->m_pPrev->m_pNext = pNewItem;
+  ASSERT(pNewItem != nullptr);
+  if (!pAfterItem) InsertLast(pNewItem); // Insert at end of list if pAfterItem is NULL
+  if (pAfterItem->m_pPrev) pAfterItem->m_pPrev->m_pNext = pNewItem;
   else m_pFirst = pNewItem;
   pNewItem->m_pNext= pAfterItem;
   pNewItem->m_pPrev = pAfterItem->m_pPrev;
@@ -105,14 +105,14 @@ void CLithBaseList<ItemType>::InsertBefore(ItemType* pAfterItem, ItemType* pNewI
 
 template<class ItemType>
 void CLithBaseList<ItemType>::Delete(ItemType* pItem) {
-  ASSERT(pItem != NULL);
-  if (pItem->m_pPrev != NULL) pItem->m_pPrev->m_pNext = pItem->m_pNext;
+  ASSERT(pItem != nullptr);
+  if (pItem->m_pPrev) pItem->m_pPrev->m_pNext = pItem->m_pNext;
   else m_pFirst = pItem->m_pNext;
-  if (pItem->m_pNext != NULL) pItem->m_pNext->m_pPrev = pItem->m_pPrev;
+  if (pItem->m_pNext) pItem->m_pNext->m_pPrev = pItem->m_pPrev;
   else m_pLast = pItem->m_pPrev;
 #ifdef _debug
-  pItem->m_pNext = NULL;
-  pItem->m_pPrev = NULL;
+  pItem->m_pNext = nullptr;
+  pItem->m_pPrev = nullptr;
 #endif
 };
 
