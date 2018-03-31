9d89bc88c40c76e17cd3d53f4426043143571596
bibendovsky <bibendovsky@hotmail.com>
2018-03-31 21:07:13 +0300

    Add wave utils class

---
+++ engine/runtime/sound/src/ltjs_wave_utils.h
@@ -0,0 +1,20 @@
+#ifndef LTJS_WAVE_UTILS_INCLUDED
+#define LTJS_WAVE_UTILS_INCLUDED
+
+
+namespace ltjs
+{
+
+
+struct WaveUtils
+{
+	static int extract_wave_size(
+		const void* raw_data);
+
+}; // WaveUtils
+
+
+} // ltjs
+
+
+#endif // LTJS_WAVE_UTILS_INCLUDED
